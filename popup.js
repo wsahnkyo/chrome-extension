92f99f08fe4080428f8dc038ea9e27c380f3f83e
Special riot <haonan.an1@se.com>
2024-06-25 14:32:57 +0800

    test

--- popup.js
+++ popup.js
@@ -1,9 +1,1 @@
-document.getElementById('captureBtn').addEventListener('click', async () => {
-    chrome.tabs.captureVisibleTab(null, {}, async (image) => {
-      // 将截图保存至本地或上传至服务器
-      const link = document.createElement('a');
-      link.href = image;
-      link.download = 'screenshot.png';
-      link.click();
-    });
-  });
+console.log("this is a popup!")
---
+++ scripts/content.js
@@ -0,0 +1,13 @@
+const body = document.querySelector("#floor-nav_557");
+console.log(body)
+const lis= body.querySelectorAll("li");
+
+lis.forEach(function(value, index) {
+    console.log(value.textContent);
+});
+
+// `document.querySelector` may return null if the selector doesn't match anything.
+if (body) {
+  const text = body.textContent;
+  console.log(text)
+}
