50fb8a7412d7dbb561a76868235f5d00dfef3ff2
Special riot <haonan.an1@se.com>
2024-06-26 16:44:13 +0800

    init

--- popup.js
+++ popup.js
@@ -1,1 +1,6 @@
 console.log("this is a popup!")
+var element = document.querySelector('.title-content-title');
+console.log(element)
+element.forEach(function(value, index) {
+    console.log(value.textContent);
+});
--- scripts/content.js
+++ scripts/content.js
@@ -1,16 +1,20 @@
-const body = document.querySelector("#floor-nav_557");
-console.log(body)
-const lis= body.querySelectorAll("li");
+const title = document.querySelector("title");
 
-lis.forEach(function(value, index) {
-    console.log(value.textContent);
-});
+console.log(title)
+console.log(title.textContent);
 
-// `document.querySelector` may return null if the selector doesn't match anything.
-if (body) {
-  const text = body.textContent;
-  console.log(text)
-}
 
-// 打开一个新窗口或标签，并导航到指定的URL
-window.open('https://www.baidu.com?'+lis[0].textContent, '_blank');
+
+// const body = document.querySelector("#floor-nav_557");
+// console.log(body)
+// const lis= body.querySelectorAll("li");
+
+// lis.forEach(function(value, index) {
+//     console.log(value.textContent);
+// });
+
+// // `document.querySelector` may return null if the selector doesn't match anything.
+// if (body) {
+//   const text = body.textContent;
+//   console.log(text)
+// }
